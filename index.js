ea491b40694d850a02e2d44ab13e25707f92feba
sashaxmorozov <a.a.morozov92@gmail.com>
2023-07-28 17:18:40 +0800

    Modules implemented

--- index.js
+++ index.js
@@ -1,69 +1,26 @@
-import Binance from 'node-binance-api';
 import {} from 'dotenv/config'
 
 let priceLuno = 0
 let priceBinance = 0
 let exchangeR = 0
 
-
-async function getPairLuno(){
-let luno
-let btcMyrLuno = 0
-const lunoob = await fetch(process.env.LUNO_API)
-luno = await lunoob.json()
-btcMyrLuno = parseFloat(luno.last_trade)
-return btcMyrLuno;
-}
-getPairLuno();
-
-async function getUsdMyr(){
-    var myHeaders = new Headers();
-    let usdMyr = 0
-myHeaders.append("apikey", process.env.EXCHANGE_API);
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
-await fetch("https://api.apilayer.com/fixer/convert?to=MYR&from=USD&amount=1", requestOptions)
-  .then(response => response.json())
-  .then(result => usdMyr = parseFloat(result.result))
-  .catch(error => console.log('error', error))
-  //console.log(usdMyr)
-  return usdMyr;
-  
-  }
-  getUsdMyr()
-
-async function getPairBinance(){
-let resBinance = 0
-const binance = new Binance()
-let ticker = await binance.prices();
-resBinance = parseFloat((`${ticker.BTCBUSD}`))
-let btcMyrBinance = 0
-let exr = 0
-let btcUsd = 0
-exr = await getUsdMyr()
-btcUsd = await getBtcUsdBinance()
-btcMyrBinance = (exr * btcUsd)
-return btcMyrBinance
-}
-
-getPairBinance()
+import { getPairBinance } from './lib/binance.js'
+import { getPairLuno } from './lib/luno.js'
+import { getPairExchange } from './lib/exchange.js'
 
 async function output(){
 let priceDiff = 0
 let percDiff = 0
 priceBinance = await getPairBinance()
 priceLuno = await getPairLuno()
-exchangeR = await getUsdMyr()
+exchangeR = await getPairExchange()
 priceDiff = priceLuno - priceBinance
-percDiff = ((priceDiff / priceBinance) * 100).toFixed(2)
-console.log("BTC/MYR on Luno: " + priceLuno)
-console.log("BTC/MYR on Binance: " + priceBinance)
-console.log("Exchange rate USD/MYR: " + exchangeR)
-console.log("Price difference: " + priceDiff)
-console.log("Luno Premium: " + percDiff + '%')
+percDiff = ((priceDiff / priceBinance) * 100)
+console.log("BTC/USD on Luno: " + priceLuno.toFixed(2))
+console.log("BTC/USD on Binance: " + priceBinance.toFixed(2))
+console.log("Exchange rate USD/MYR: " + exchangeR.toFixed(2))
+console.log("Price difference: " + priceDiff.toFixed(2))
+console.log("Luno Premium: " + percDiff.toFixed(2) + '%')
 }
 
 output()
